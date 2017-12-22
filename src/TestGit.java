d2bf5de01b31e68df649491b6bbabe69a9b277f6
prepareForwf <15313319805@163.com>
2017-12-22 11:33:03 +0800

    测试git提交新增

--- src/TestGit.java
+++ src/TestGit.java
@@ -4,6 +4,6 @@
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello world!");
-        System.out.println("Hel lo World!");
+        System.out.println("Hello World!");
     }
 }
---
+++ src/TestNewGit.java
@@ -0,0 +1,8 @@
+/**
+ * Created by dongao on 2017/12/22.
+ */
+public class TestNewGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
